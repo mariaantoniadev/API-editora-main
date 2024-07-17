2316243d4b7587e0d06aa4ee73432ced6fe26e6c
Rodrigo <rodrigo.a42@gmail.com>
2024-07-17 13:24:34 -0300

    botao_enviar

--- frontend/src/app/components/Titulo.tsx
+++ frontend/src/app/components/Titulo.tsx
@@ -58,6 +58,14 @@ function Titulo() {
               <span onClick={logout} style={{ cursor: "pointer" }}>
                 <RxExit className="inline" title="Sair" />
               </span>
+              <Link href="http://localhost:3002/">
+                <button
+                  type="submit"
+                  className="text-orange-700 absolute end-2.5 bottom-2.5 border border-orange-700 hover:bg-orange-100 hover:text-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 dark:border-orange-600 dark:text-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+                >
+                  Enviar livro
+                </button>
+              </Link>
             </div>
           ) : (
             <Link
